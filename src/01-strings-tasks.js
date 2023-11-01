168ecdc35ed6492131e77c77da31e4791bc21ce7
Sasha Karpovich <roobel2005@gmail.com>
2023-11-01 16:40:05 +0300

    extractEmails

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -192,8 +192,9 @@ function convertToUpperCase(str) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  return str.split(';')
+    .map((email) => email.trim());
 }
 
 /**
