a18cc9da9f1b5c4dd853e3b30dd4f7895ed07a21
Sasha Karpovich <roobel2005@gmail.com>
2023-11-04 15:59:09 +0300

    logger

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -149,8 +149,18 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function wrappedFunction(...args) {
+    const argsString = args.map((a) => JSON.stringify(a))
+      .join(',');
+    const functionName = func.name || 'anonymous';
+
+    logFunc(`${functionName}(${argsString}) starts`);
+    const result = func.apply(this, args);
+    logFunc(`${functionName}(${argsString}) ends`);
+
+    return result;
+  };
 }
 
 /**
