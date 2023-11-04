5e5c870b507465ee07420c11e2ef9bbd4daba9d8
Sasha Karpovich <roobel2005@gmail.com>
2023-11-04 15:51:51 +0300

    getPolynom

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -59,8 +59,13 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...coefficients) {
+  if (coefficients.length === 0) return null;
+
+  return (x) => coefficients.reduce((acc, curr, index) => {
+    const power = coefficients.length - index - 1;
+    return acc + curr * x ** power;
+  }, 0);
 }
 
 /**
